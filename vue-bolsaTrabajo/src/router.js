6e56a55e0396670ba0dfee1a4892070cf7735ac4
Diego <kleiman.diego@gmail.com>
2024-11-18 20:32:52 -0300

    Implementado GithubPages

--- vue-bolsaTrabajo/src/router.js
+++ vue-bolsaTrabajo/src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "./components/Home.vue";
 import UserRegistration from "./components/UserRegistration.vue";
 import UserLogin from "./components/UserLogin.vue";
@@ -28,7 +28,7 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(), // Cambia de createWebHistory() a createWebHashHistory()
 	routes,
 });
 
